c5f9cb4d662c3db28665c96c33bdfe0dcaa7e3d0
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-11-02 05:30:33 -0500

    sorted all event handlers, labelled each one.

--- game.cpp
+++ game.cpp
@@ -12,6 +12,13 @@ Game::~Game()
 {
     delete ui;
 }
+/*---------------------
+ Rows 1-8 event handlers
+To quick search, CTRL+F
+Then type for ex.:
+"Collumn 1 Row 8"
+Each handler is commented.
+----------------------*/
 /*=====================
  * Row 1
  ====================*/
@@ -23,6 +30,627 @@ void Game::on_Coll1R1_clicked()
 
 void Game::on_Coll2R1_clicked()
 {
-
+    //Collumn 2 Row 1
 }
+
+void Game::on_Coll3R1_clicked()
+{
+    //Collumn 3 Row 1
+}
+
+
+void Game::on_Coll4R1_clicked()
+{
+    //Collumn 4 Row 1
+}
+
+
+void Game::on_Coll5R1_clicked()
+{
+    //Collumn 5 Row 1
+}
+
+
+void Game::on_Coll6R1_clicked()
+{
+    //Collumn 6 Row 1
+}
+
+
+void Game::on_Coll7R1_clicked()
+{
+    //Collumn 7 Row 1
+}
+
+
+void Game::on_Coll8R1_clicked()
+{
+    //Collumn 8 Row 1
+}
+
+
+void Game::on_Coll9R1_clicked()
+{
+    //Collumn 9 Row 1
+}
+
+
+void Game::on_Coll10R1_clicked()
+{
+    //Collumn 10 Row 1
+}
+
+
+void Game::on_Coll11R1_clicked()
+{
+    //Collumn 11 Row 1
+}
+
+
+void Game::on_Coll12R1_clicked()
+{
+    //Collumn 12 Row 1
+}
+
+void Game::on_Coll13R1_clicked()
+{
+    //Collumn 13 Row 1
+}
+/*=====================
+ * Row 2
+ ====================*/
+void Game::on_Coll1R2_clicked()
+{
+    //Collumn 1 Row 2
+}
+void Game::on_Coll2R2_clicked()
+{
+    //Collumn 2 Row 2
+}
+
+void Game::on_Coll3R2_clicked()
+{
+    //Collumn 3 Row 2
+}
+
+
+void Game::on_Coll4R2_clicked()
+{
+    //Collumn 4 Row 2
+}
+
+
+void Game::on_Coll5R2_clicked()
+{
+    //Collumn 5 Row 2
+}
+
+
+void Game::on_Coll6R2_clicked()
+{
+    //Collumn 6 Row 2
+}
+
+
+void Game::on_Coll7R2_clicked()
+{
+    //Collumn 7 Row 2
+}
+
+
+void Game::on_Coll8R2_clicked()
+{
+    //Collumn 8 Row 2
+}
+
+
+void Game::on_Coll9R2_clicked()
+{
+    //Collumn 9 Row 2
+}
+
+
+void Game::on_Coll10R2_clicked()
+{
+    //Collumn 10 Row 2
+}
+
+
+void Game::on_Coll11R2_clicked()
+{
+    //Collumn 11 Row 2
+}
+
+
+void Game::on_Coll12R2_clicked()
+{
+    //Collumn 12 Row 2
+}
+
+
+void Game::on_Coll13R2_clicked()
+{
+    //Collumn 13 Row 2
+}
+
+/*=====================
+ * Row 3
+ ====================*/
+void Game::on_Coll1R3_clicked()
+{
+    //Collumn 1 Row 3
+}
+
+
+void Game::on_Coll2R3_clicked()
+{
+    //Collumn 2 Row 3
+}
+
+
+void Game::on_Coll3R3_clicked()
+{
+    //Collumn 3 Row 3
+}
+
+
+void Game::on_Coll4R3_clicked()
+{
+    //Collumn 4 Row 3
+}
+
+
+void Game::on_Coll5R3_clicked()
+{
+    //Collumn 5 Row 3
+}
+
+
+void Game::on_Coll6R3_clicked()
+{
+    //Collumn 6 Row 3
+}
+
+
+void Game::on_Coll7R3_clicked()
+{
+    //Collumn 7 Row 3
+}
+
+
+void Game::on_Coll8R3_clicked()
+{
+    //Collumn 8 Row 3
+}
+
+
+void Game::on_Coll9R3_clicked()
+{
+    //Collumn 9 Row 3
+}
+
+
+void Game::on_Coll10R3_clicked()
+{
+    //Collumn 10 Row 3
+}
+
+
+void Game::on_Coll11R3_clicked()
+{
+    //Collumn 11 Row 3
+}
+
+
+void Game::on_Coll12R3_clicked()
+{
+    //Collumn 12 Row 3
+}
+
+
+void Game::on_Coll13R3_clicked()
+{
+    //Collumn 13 Row 3
+}
+
+/*=====================
+ * Row 4
+ ====================*/
+void Game::on_Coll1R4_clicked()
+{
+    //Collumn 1 Row 4
+}
+
+
+void Game::on_Coll2R4_clicked()
+{
+    //Collumn 2 Row 4
+}
+
+
+void Game::on_Coll3R4_clicked()
+{
+    //Collumn 3 Row 4
+}
+
+
+void Game::on_Coll4R4_clicked()
+{
+    //Collumn 4 Row 4
+}
+
+
+void Game::on_Coll5R4_clicked()
+{
+    //Collumn 5 Row 4
+}
+
+
+void Game::on_Coll6R4_clicked()
+{
+    //Collumn 6 Row 4
+}
+
+
+void Game::on_Coll7R4_clicked()
+{
+    //Collumn 7 Row 4
+}
+
+
+void Game::on_Coll8R4_clicked()
+{
+    //Collumn 8 Row 4
+}
+
+
+void Game::on_Coll9R4_clicked()
+{
+    //Collumn 9 Row 4
+}
+
+
+void Game::on_Coll10R4_clicked()
+{
+    //Collumn 10 Row 4
+}
+
+
+void Game::on_Coll11R4_clicked()
+{
+    //Collumn 11 Row 4
+}
+
+
+void Game::on_Coll12R4_clicked()
+{
+    //Collumn 12 Row 4
+}
+
+
+void Game::on_Coll13R4_clicked()
+{
+    //Collumn 13 Row 4
+}
+/*=====================
+ * Row 5
+ ====================*/
+
+void Game::on_Coll1R5_clicked()
+{
+    //Collumn 1 Row 5
+}
+
+
+void Game::on_Coll2R5_clicked()
+{
+    //Collumn 2 Row 5
+}
+
+
+void Game::on_Coll3R5_clicked()
+{
+    //Collumn 3 Row 5
+}
+
+
+void Game::on_Coll4R5_clicked()
+{
+    //Collumn 4 Row 5
+}
+
+
+void Game::on_Coll5R5_clicked()
+{
+    //Collumn 5 Row 5
+}
+
+
+void Game::on_Coll6R5_clicked()
+{
+    //Collumn 6 Row 5
+}
+
+
+void Game::on_Coll7R5_clicked()
+{
+    //Collumn 7 Row 5
+}
+
+
+void Game::on_Coll8R5_clicked()
+{
+    //Collumn 8 Row 5
+}
+
+
+void Game::on_Coll9R5_clicked()
+{
+    //Collumn 9 Row 5
+}
+
+
+void Game::on_Coll10R5_clicked()
+{
+    //Collumn 10 Row 5
+}
+
+
+void Game::on_Coll11R5_clicked()
+{
+    //Collumn 11 Row 5
+}
+
+
+void Game::on_Coll12R5_clicked()
+{
+    //Collumn 12 Row 5
+}
+
+
+void Game::on_Coll13R5_clicked()
+{
+    //Collumn 13 Row 5
+}
+/*=====================
+ * Row 6
+ ====================*/
+
+void Game::on_Coll1R6_clicked()
+{
+    //Collumn 1 Row 6
+}
+
+
+void Game::on_Coll2R6_clicked()
+{
+    //Collumn 2 Row 6
+}
+
+
+void Game::on_Coll3R6_clicked()
+{
+    //Collumn 3 Row 6
+}
+
+
+void Game::on_Coll4R6_clicked()
+{
+    //Collumn 4 Row 6
+}
+
+
+void Game::on_Coll5R6_clicked()
+{
+    //Collumn 5 Row 6
+}
+
+
+void Game::on_Coll6R6_clicked()
+{
+    //Collumn 6 Row 6
+}
+
+
+void Game::on_Coll7R6_clicked()
+{
+    //Collumn 7 Row 6
+}
+
+
+void Game::on_Coll8R6_clicked()
+{
+    //Collumn 8 Row 6
+}
+
+
+void Game::on_Coll9R6_clicked()
+{
+    //Collumn 9 Row 6
+}
+
+
+void Game::on_Coll10R6_clicked()
+{
+    //Collumn 10 Row 6
+}
+
+
+void Game::on_Coll11R6_clicked()
+{
+    //Collumn 11 Row 6
+}
+
+
+void Game::on_Coll12R6_clicked()
+{
+    //Collumn 12 Row 6
+}
+
+
+void Game::on_Coll13R6_clicked()
+{
+    //Collumn 13 Row 6
+}
+/*=====================
+ * Row 7
+ ====================*/
+
+void Game::on_Coll1R7_clicked()
+{
+    //Collumn 1 Row 7
+}
+
+
+void Game::on_Coll2R7_clicked()
+{
+    //Collumn 2 Row 7
+}
+
+
+void Game::on_Coll3R7_clicked()
+{
+    //Collumn 3 Row 7
+}
+
+
+void Game::on_Coll4R7_clicked()
+{
+    //Collumn 4 Row 7
+}
+
+
+void Game::on_Coll5R7_clicked()
+{
+    //Collumn 5 Row 7
+}
+
+
+void Game::on_Coll6R7_clicked()
+{
+    //Collumn 6 Row 7
+}
+
+
+void Game::on_Coll7R7_clicked()
+{
+    //Collumn 7 Row 7
+}
+
+
+void Game::on_Coll8R7_clicked()
+{
+    //Collumn 8 Row 7
+}
+
+
+void Game::on_Coll9R7_clicked()
+{
+    //Collumn 9 Row 7
+}
+
+
+void Game::on_Coll10R7_clicked()
+{
+    //Collumn 10 Row 7
+}
+
+
+void Game::on_Coll11R7_clicked()
+{
+    //Collumn 11 Row 7
+}
+
+
+void Game::on_Coll12R7_clicked()
+{
+    //Collumn 12 Row 7
+}
+
+
+void Game::on_Coll13R7_clicked()
+{
+    //Collumn 13 Row 7
+}
+/*=====================
+ * Row 8
+ ====================*/
+
+void Game::on_Coll1R8_clicked()
+{
+    //Collumn 1 Row 8
+}
+
+
+void Game::on_Coll2R8_clicked()
+{
+    //Collumn 2 Row 8
+}
+
+
+void Game::on_Coll3R8_clicked()
+{
+    //Collumn 3 Row 8
+}
+
+
+void Game::on_Coll4R8_clicked()
+{
+    //Collumn 4 Row 8
+}
+
+
+void Game::on_Coll5R8_clicked()
+{
+    //Collumn 5 Row 8
+}
+
+
+void Game::on_Coll6R8_clicked()
+{
+    //Collumn 6 Row 8
+}
+
+
+void Game::on_Coll7R8_clicked()
+{
+    //Collumn 7 Row 8
+}
+
+
+void Game::on_Coll8R8_clicked()
+{
+    //Collumn 8 Row 8
+}
+
+
+void Game::on_Coll9R8_clicked()
+{
+    //Collumn 9 Row 8
+}
+
+
+void Game::on_Coll10R8_clicked()
+{
+    //Collumn 10 Row 8
+}
+
+
+void Game::on_Coll11R8_clicked()
+{
+    //Collumn 11 Row 8
+}
+
+
+void Game::on_Coll12R8_clicked()
+{
+    //Collumn 12 Row 8
+}
+
+
+void Game::on_Coll13R8_clicked()
+{
+    //Collumn 13 Row 8
+}
+
 
--- game.h
+++ game.h
@@ -15,6 +15,217 @@ public:
     explicit Game(QWidget *parent = nullptr);
     ~Game();
 
+private slots:
+    void on_Coll1R1_clicked();
+
+    void on_Coll2R1_clicked();
+
+    void on_Coll13R1_clicked();
+
+    void on_Coll3R1_clicked();
+
+    void on_Coll4R1_clicked();
+
+    void on_Coll5R1_clicked();
+
+    void on_Coll6R1_clicked();
+
+    void on_Coll7R1_clicked();
+
+    void on_Coll8R1_clicked();
+
+    void on_Coll9R1_clicked();
+
+    void on_Coll10R1_clicked();
+
+    void on_Coll11R1_clicked();
+
+    void on_Coll12R1_clicked();
+
+    void on_Coll1R2_clicked();
+
+    void on_Coll2_clicked();
+
+    void on_Coll3R2_clicked();
+
+    void on_Coll4R2_clicked();
+
+    void on_Coll5R2_clicked();
+
+    void on_Coll6R2_clicked();
+
+    void on_Coll7R2_clicked();
+
+    void on_Coll8R2_clicked();
+
+    void on_Coll9R2_clicked();
+
+    void on_Coll10R2_clicked();
+
+    void on_Coll11R2_clicked();
+
+    void on_Coll12R2_clicked();
+
+    void on_Coll13R2_clicked();
+
+    void on_Coll1R3_clicked();
+
+    void on_Coll2R3_clicked();
+
+    void on_Coll3R3_clicked();
+
+    void on_Coll4R3_clicked();
+
+    void on_Coll5R3_clicked();
+
+    void on_Coll6R3_clicked();
+
+    void on_Coll7R3_clicked();
+
+    void on_Coll8R3_clicked();
+
+    void on_Coll9R3_clicked();
+
+    void on_Coll10R3_clicked();
+
+    void on_Coll11R3_clicked();
+
+    void on_Coll12R3_clicked();
+
+    void on_Coll13R3_clicked();
+
+    void on_Coll1R4_clicked();
+
+    void on_Coll2R4_clicked();
+
+    void on_Coll3R4_clicked();
+
+    void on_Coll4R4_clicked();
+
+    void on_Coll5R4_clicked();
+
+    void on_Coll6R4_clicked();
+
+    void on_Coll7R4_clicked();
+
+    void on_Coll8R4_clicked();
+
+    void on_Coll9R4_clicked();
+
+    void on_Coll10R4_clicked();
+
+    void on_Coll11R4_clicked();
+
+    void on_Coll12R4_clicked();
+
+    void on_Coll13R4_clicked();
+
+    void on_Coll1R5_clicked();
+
+    void on_Coll2R5_clicked();
+
+    void on_Coll3R5_clicked();
+
+    void on_Coll4R5_clicked();
+
+    void on_Coll5R5_clicked();
+
+    void on_Coll6R5_clicked();
+
+    void on_Coll7R5_clicked();
+
+    void on_Coll8R5_clicked();
+
+    void on_Coll9R5_clicked();
+
+    void on_Coll10R5_clicked();
+
+    void on_Coll11R5_clicked();
+
+    void on_Coll12R5_clicked();
+
+    void on_Coll13R5_clicked();
+
+    void on_Coll1R6_clicked();
+
+    void on_Coll2R6_clicked();
+
+    void on_Coll3R6_clicked();
+
+    void on_Coll4R6_clicked();
+
+    void on_Coll5R6_clicked();
+
+    void on_Coll6R6_clicked();
+
+    void on_Coll7R6_clicked();
+
+    void on_Coll8R6_clicked();
+
+    void on_Coll9R6_clicked();
+
+    void on_Coll10R6_clicked();
+
+    void on_Coll11R6_clicked();
+
+    void on_Coll12R6_clicked();
+
+    void on_Coll13R6_clicked();
+
+    void on_Coll1R7_clicked();
+
+    void on_Coll2R7_clicked();
+
+    void on_Coll3R7_clicked();
+
+    void on_Coll4R7_clicked();
+
+    void on_Coll5R7_clicked();
+
+    void on_Coll6R7_clicked();
+
+    void on_Coll7R7_clicked();
+
+    void on_Coll8R7_clicked();
+
+    void on_Coll9R7_clicked();
+
+    void on_Coll10R7_clicked();
+
+    void on_Coll11R7_clicked();
+
+    void on_Coll12R7_clicked();
+
+    void on_Coll13R7_clicked();
+
+    void on_Coll1R8_clicked();
+
+    void on_Coll2R8_clicked();
+
+    void on_Coll3R8_clicked();
+
+    void on_Coll4R8_clicked();
+
+    void on_Coll5R8_clicked();
+
+    void on_Coll6R8_clicked();
+
+    void on_Coll7R8_clicked();
+
+    void on_Coll8R8_clicked();
+
+    void on_Coll9R8_clicked();
+
+    void on_Coll10R8_clicked();
+
+    void on_Coll11R8_clicked();
+
+    void on_Coll12R8_clicked();
+
+    void on_Coll13R8_clicked();
+
+    void on_Coll2R2_clicked();
+
 private:
     Ui::Game *ui;
 };
