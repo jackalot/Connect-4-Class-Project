c2eb2d3c81f54923dc974d2475db21554a449c1d
Arcane-Blaze <rkaintech@outlook.com>
2025-11-15 17:18:13 -0500

    Update game.h
    Qt gave me errors when committing so committing every file of working code before pull request

--- game.h
+++ game.h
@@ -2,8 +2,12 @@
 #define GAME_H
 #include <QPushButton>
 #include <QDialog>
-#include <string>
+#include <QRegularExpression>
 using namespace std;
+
+//Declare Connect4Board class
+class Connect4Board;
+
 namespace Ui {
 class Game;
 }
@@ -14,243 +18,19 @@ class Game : public QDialog
 
 public:
     explicit Game(QWidget *parent = nullptr);
-    void setSinglePlayer(bool singlePlayer);
-    void HighlightCell(int column, int Row, char ColorKey)
-{
-        // Convert column and Row to strings before concatenating
-        string cellName = "Coll" + std::to_string(column) + "R" + std::to_string(Row);
-        QPushButton* button = this->findChild<QPushButton*>(cellName.c_str());
-        if (button) {
-            if(ColorKey == 'B')
-            {
-                button->setStyleSheet("background-color: blue; color: white;");
-            }
-            else if(ColorKey == 'R') {
-                button->setStyleSheet("background-color: red; color: white;");
-            }
-        }
-    }
-        void DropInColumn(int column, int row); // This should match the definition
+    void HighlightCell(int column, int Row, char ColorKey);
     ~Game();
 
 private slots:
-    void on_Coll1R1_clicked();
 
-    void on_Coll2R1_clicked();
-
-    void on_Coll13R1_clicked();
-
-    void on_Coll3R1_clicked();
-
-    void on_Coll4R1_clicked();
-
-    void on_Coll5R1_clicked();
-
-    void on_Coll6R1_clicked();
-
-    void on_Coll7R1_clicked();
-
-    void on_Coll8R1_clicked();
-
-    void on_Coll9R1_clicked();
-
-    void on_Coll10R1_clicked();
-
-    void on_Coll11R1_clicked();
-
-    void on_Coll12R1_clicked();
-
-    void on_Coll1R2_clicked();
-
-    //void on_Coll2_clicked();
-
-    void on_Coll3R2_clicked();
-
-    void on_Coll4R2_clicked();
-
-    void on_Coll5R2_clicked();
-
-    void on_Coll6R2_clicked();
-
-    void on_Coll7R2_clicked();
-
-    void on_Coll8R2_clicked();
-
-    void on_Coll9R2_clicked();
-
-    void on_Coll10R2_clicked();
-
-    void on_Coll11R2_clicked();
-
-    void on_Coll12R2_clicked();
-
-    void on_Coll13R2_clicked();
-
-    void on_Coll1R3_clicked();
-
-    void on_Coll2R3_clicked();
-
-    void on_Coll3R3_clicked();
-
-    void on_Coll4R3_clicked();
-
-    void on_Coll5R3_clicked();
-
-    void on_Coll6R3_clicked();
-
-    void on_Coll7R3_clicked();
-
-    void on_Coll8R3_clicked();
-
-    void on_Coll9R3_clicked();
-
-    void on_Coll10R3_clicked();
-
-    void on_Coll11R3_clicked();
-
-    void on_Coll12R3_clicked();
-
-    void on_Coll13R3_clicked();
-
-    void on_Coll1R4_clicked();
-
-    void on_Coll2R4_clicked();
-
-    void on_Coll3R4_clicked();
-
-    void on_Coll4R4_clicked();
-
-    void on_Coll5R4_clicked();
-
-    void on_Coll6R4_clicked();
-
-    void on_Coll7R4_clicked();
-
-    void on_Coll8R4_clicked();
-
-    void on_Coll9R4_clicked();
-
-    void on_Coll10R4_clicked();
-
-    void on_Coll11R4_clicked();
-
-    void on_Coll12R4_clicked();
-
-    void on_Coll13R4_clicked();
-
-    void on_Coll1R5_clicked();
-
-    void on_Coll2R5_clicked();
-
-    void on_Coll3R5_clicked();
-
-    void on_Coll4R5_clicked();
-
-    void on_Coll5R5_clicked();
-
-    void on_Coll6R5_clicked();
-
-    void on_Coll7R5_clicked();
-
-    void on_Coll8R5_clicked();
-
-    void on_Coll9R5_clicked();
-
-    void on_Coll10R5_clicked();
-
-    void on_Coll11R5_clicked();
-
-    void on_Coll12R5_clicked();
-
-    void on_Coll13R5_clicked();
-
-    void on_Coll1R6_clicked();
-
-    void on_Coll2R6_clicked();
-
-    void on_Coll3R6_clicked();
-
-    void on_Coll4R6_clicked();
-
-    void on_Coll5R6_clicked();
-
-    void on_Coll6R6_clicked();
-
-    void on_Coll7R6_clicked();
-
-    void on_Coll8R6_clicked();
-
-    void on_Coll9R6_clicked();
-
-    void on_Coll10R6_clicked();
-
-    void on_Coll11R6_clicked();
-
-    void on_Coll12R6_clicked();
-
-    void on_Coll13R6_clicked();
-
-    void on_Coll1R7_clicked();
-
-    void on_Coll2R7_clicked();
-
-    void on_Coll3R7_clicked();
-
-    void on_Coll4R7_clicked();
-
-    void on_Coll5R7_clicked();
-
-    void on_Coll6R7_clicked();
-
-    void on_Coll7R7_clicked();
-
-    void on_Coll8R7_clicked();
-
-    void on_Coll9R7_clicked();
-
-    void on_Coll10R7_clicked();
-
-    void on_Coll11R7_clicked();
-
-    void on_Coll12R7_clicked();
-
-    void on_Coll13R7_clicked();
-
-    void on_Coll1R8_clicked();
-
-    void on_Coll2R8_clicked();
-
-    void on_Coll3R8_clicked();
-
-    void on_Coll4R8_clicked();
-
-    void on_Coll5R8_clicked();
-
-    void on_Coll6R8_clicked();
-
-    void on_Coll7R8_clicked();
-
-    void on_Coll8R8_clicked();
-
-    void on_Coll9R8_clicked();
-
-    void on_Coll10R8_clicked();
-
-    void on_Coll11R8_clicked();
-
-    void on_Coll12R8_clicked();
-
-    void on_Coll13R8_clicked();
-
-    void on_Coll2R2_clicked();
+    void onGridCellClicked();
 
 private:
-    void resetBoard();
     Ui::Game *ui;
+    Connect4Board* ourGameBoard;
+    char currentPlayerPiece;
 
-    bool m_singlePlayer = false;   // true = One Player, false = Two Players
-    int  m_currentPlayer = 1;      // 1 = human player, 2 = AI or player 2
-    bool m_gameOver = false;       // true when there is a winner or tie
+    void DropInColumn(int column);
 };
 
 
