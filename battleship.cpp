9a2eb5a78af3d2a1d2d0ec696a0204b0a1a9ff1f
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-11-20 16:20:04 -0500

    added regex to match the currently selected button

--- battleship.cpp
+++ battleship.cpp
@@ -37,8 +37,13 @@ void battleship::onButtonClicked() {
     QPushButton* button = qobject_cast<QPushButton*>(sender());
     if (button) {
         QString buttonName = button->objectName();
+        //Use regex to parse out row and collumn
+        static QRegularExpression regex("Coll(\\d+)R(\\d+)");
+        QRegularExpressionMatch match = regex.match(buttonName);
+
         // Your code based on buttonName
         int col = match.captured(1).toInt();
+        int row = match.captured(2).toInt();
         HighlightCell(1, 1);
     }
 }
