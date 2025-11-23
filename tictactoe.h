b69af8298a0e6491490f0285ad7fa1b2a655cc5b
Arcane-Blaze <rkaintech@outlook.com>
2025-11-22 21:55:41 -0500

    Create tictactoe.h added tic tac toe

---
+++ tictactoe.h
@@ -0,0 +1,55 @@
+#ifndef TICTACTOE_H
+#define TICTACTOE_H
+#include <QDialog>
+#include <QPushButton>
+#include "boardgrid.h"
+using namespace std;
+
+namespace Ui {
+class TicTacToe;
+}
+
+// Applying inheritance for board grid
+class TicTacToeBoard : public BoardGrid {
+public:
+    TicTacToeBoard() : BoardGrid(3, 3) {}
+
+    bool checkWin(char p) {
+        // Check Rows & Columns for wins
+        for (int i = 0; i < 3; i++) {
+            if (getCell(i, 0) == p && getCell(i, 1) == p && getCell(i, 2) == p) return true;
+            if (getCell(0, i) == p && getCell(1, i) == p && getCell(2, i) == p) return true;
+        }
+        // Check diagonals for wins
+        if (getCell(0, 0) == p && getCell(1, 1) == p && getCell(2, 2) == p) return true;
+        if (getCell(0, 2) == p && getCell(1, 1) == p && getCell(2, 0) == p) return true;
+        return false;
+    }
+};
+
+class TicTacToe : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit TicTacToe(QWidget *parent = nullptr, bool singlePlayer = false);
+    ~TicTacToe();
+
+private slots:
+    void onGridCellClicked();
+    void on_resetButton_clicked();
+
+private:
+    Ui::TicTacToe *ui;
+    TicTacToeBoard* board;
+    bool isSinglePlayer;
+    char currentPlayer;
+    bool gameOver;
+
+    void handleMove(int col, int row);
+    void aiMove();
+    void updateUI(int col, int row, char player);
+    void checkGameState(char player);
+};
+
+#endif
